5ba5eb370a1ff8f127accfdb541b7da16f09e9da
yanyan7 <sakuraperfection@gmail.com>
2021-01-19 19:35:34 +0900

    完了

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,3 +1,4 @@
 class Follow < ApplicationRecord
   belongs_to :from_user, class_name: "User", foreign_key: "from_user_id"
+  belongs_to :to_user, class_name: "User", foreign_key: "to_user_id"
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
-  has_many :follows, foreign_key: :from_user_id
+  has_many :follows_from, class_name: "Follow", foreign_key: :from_user_id, dependent: :destroy
+  has_many :follows_to, class_name: "Follow", foreign_key: :to_user_id, dependent: :destroy
+  has_many :following, through: :follows_from, source: :to_user
+  has_many :followed, through: :follows_to, source: :from_user
 end
